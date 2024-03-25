8065d13d04e4a2ae07e97c56c1ded72789994bb2
Azure Pipeline <azure-pipeline@coso.com>
2024-03-25 14:56:24 +0100

    add openai

--- src/main/kotlin/furhatos/app/furhatwhoami/flow/init.kt
+++ src/main/kotlin/furhatos/app/furhatwhoami/flow/init.kt
@@ -2,6 +2,7 @@ package furhatos.app.furhatwhoami.flow
 
 import furhatos.app.furhatwhoami.flow.main.Idle
 import furhatos.app.furhatwhoami.flow.main.Greeting
+import furhatos.app.furhatwhoami.flow.main.Openai
 import furhatos.app.furhatwhoami.setting.DISTANCE_TO_ENGAGE
 import furhatos.app.furhatwhoami.setting.MAX_NUMBER_OF_USERS
 import furhatos.flow.kotlin.State
@@ -17,10 +18,10 @@ val Init: State = state {
     onEntry {
         /** start interaction */
         when {
-            furhat.isVirtual() -> goto(Greeting) // Convenient to bypass the need for user when running Virtual Furhat
+            furhat.isVirtual() -> goto(Openai) // Convenient to bypass the need for user when running Virtual Furhat
             users.hasAny() -> {
                 furhat.attend(users.random)
-                goto(Greeting)
+                goto(Openai)
             }
             else -> goto(Idle)
         }
